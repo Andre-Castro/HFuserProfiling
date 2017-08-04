3464e95f47a0d464e55b8df0e934e0a5721b374d
Andre-Castro <acast050@ucr.edu>
2017-08-03 18:39:18 -0700

    Add files via upload

--- test2.py
+++ test2.py
@@ -6,12 +6,18 @@ import numpy as np
 import csv
 import operator
 import time
+import pickle
+#from wordcount import clean
+from wordcloud import WordCloud
+import psycopg2
+import re
+from bs4 import BeautifulSoup
+
 
 # NOTE TO SELF:
 # FALSE == 0, TRUE != 0
 
-
-def buildGraph(sizeLimit=False, limitNum=500):
+def buildGraph(sizeLimit=False, limitNum=500, save=False):
     usernameDict = {}
     tidDict = {}
     vertex_counter = 0
@@ -69,33 +75,122 @@ def buildGraph(sizeLimit=False, limitNum=500):
     # plot(g, layout = layout, vertex_color=["red" if type == 0 else "blue" for type in g.vs["type"]], edge_width = [weight for weight in g.es["weight"]])
     # plot(g, vertex_color=["red" if type == 0 else "blue" for type in g.vs["type"]], edge_width = [weight for weight in g.es["weight"]])
 
+    if save:
+        g.write_pickle('save.pickle', version=2)
+        # pickle.dump(g, open("save.p", "wb"))
+        print("Graph saves to pickle file!")
+    
     return g
     
 
+#test function for me for misc purposes
+def algoTest(g, func=None):
+    if func == "fastGreedy":
+        dendro = g.community_fastgreedy(weights="weight")
+        # print(dendro)
+        summary(dendro)
+        print(dendro.optimal_count)
+        dendroClusters = dendro.as_clustering(n=6)
+        print(len(dendroClusters))
+        
+        # dendroClustersSorted = sorted(dendroClusters, key = len, reverse = True)
+        '''
+        file = open('fastGreedy_cluster_list_Weights.txt', 'w')
+        file.write("List of clusters using fastgreedy algoirthm with weights:\n")
+        file.write("-----------------------------------------------------\n")
+        for item in dendroClustersSorted:
+            # file.write(str(item) + "\n")
+            writeStr = ""
+            for id in item:
+                writeStr += g.vs[id]["idx"] + " "
+            file.write(writeStr + "\n")
+                
+        file = open('fastGreedy_cluster_lenghts.txt', 'w')
+        for cluster in dendroClustersSorted:
+            file.write(str(len(cluster)) + "\n")
+        '''
+    elif func == "Top1%":
+        #top 1% of degree code:
+        degrees_of_users = []
+        for i, vertex in enumerate(g.vs):
+            #print(i, vertex)
+            if g.vs[i]["type"] == 1:
+                degrees_of_users.append((vertex["idx"], g.degree(i)))
+                
+        # degrees_of_users.sort(key=lambda x: x[1]) ......sorting method below is faster?
+        degrees_of_users.sort(key=operator.itemgetter(1), reverse=True)
+        topPercentNum = int(len(degrees_of_users)/100)
+        
+        print("Top 1% of threads with highest degree")
+        for i in range(topPercentNum):
+            print(str(i) + ") " + str(degrees_of_users[i]))
+        
+        # double check result
+        # print("All users degree:")
+        # for element in degrees_of_users:
+            # print(element[1])
             
-def algoTest(g):
-    
-    dendro = g.community_fastgreedy(weights="weight")
-    # print(dendro)
-    summary(dendro)
-    print(dendro.optimal_count)
-    dendroClusters = dendro.as_clustering()
-    dendroClustersSorted = sorted(dendroClusters, key = len, reverse = True)
-
-    file = open('fastGreedy_cluster_list_Weights.txt', 'w')
-    file.write("List of clusters using fastgreedy algoirthm with weights:\n")
-    file.write("-----------------------------------------------------\n")
-    for item in dendroClustersSorted:
-        # file.write(str(item) + "\n")
-        writeStr = ""
-        for id in item:
-            writeStr += g.vs[id]["idx"] + " "
-        file.write(writeStr + "\n")
+    elif func == "getPostContentFromEdgeList":
+        DBname = "MichalisPrj"
+        pword = "1"
+        tableName = "tbl_hackthissite_post"
+        con = psycopg2.connect(database=DBname, user = "postgres", password=pword) #connect to postgres
+        curs = con.cursor()
+        
+        for i in range(1,7):
+        
+            file = open('graph-and-edges\select_six_dege_{}.txt'.format(i), 'r')
+            clusterEdgeList = file.read().split('\n')
             
-    file = open('fastGreedy_cluster_lenghts.txt', 'w')
-    for cluster in dendroClustersSorted:
-        file.write(str(len(cluster)) + "\n")
+            file = open('select_six_post_content_{}.txt'.format(i), 'wb')
+            
+            for edge in clusterEdgeList:
+                edgePair = sorted(edge.split(','))
+                if len(edgePair) == 2:
+                    tid = edgePair[0]
+                    uid = edgePair[1]
+                    # print(uid, tid)
+                    
+                    ExecuteStatement = "SELECT post_content FROM " + tableName + " WHERE uid = '{}' ".format(uid[4:]) + "AND tid = '{}'".format(tid[4:])
+                    #when searching for a uid and tid, all posts that that user has in that forum is considered. 
+                    #for exapme: uid = 6425 and tid = 212 has 9 posts in total.
+                    #they are all taken into account when analyzing the topic of 'select_six_dege_1.txt'
+                    curs.execute(ExecuteStatement)
+                    postList = curs.fetchall()
+                    for post in postList:
+                    # print(str(post_content))
+                    # print("length: " + str(len(post_content)))
+                    # print("uid: " + str(uid))
+                    # print("tid: " + str(tid))
+                        cleanStr = clean(str(post))
+                        cleanStr = cleanStr[3:-4]
+                        # cleanStr = str(cleanStr.encode("utf-8")).
+                        #Remove the unicode space in the file
+                        file.write(bytes(cleanStr + "\n", encoding="utf-8"))
+            
+            print("Done with {}".format(i))
     
+    elif func == "wordcloud":
+         for i in range(1,7):
+            file = open('select_six_post_content_{}.txt'.format(i), 'r', encoding="utf-8")
+            stopWordsFile = open('stopwords.txt', 'r')
+            
+            fig = plt.figure(figsize=(20, 10))
+            ax  = fig.add_subplot('111')
+            
+            wcloud = WordCloud(background_color="white", mode = "RGB", width=2000, height = 1000, stopwords = stopWordsFile.read().split('\n')).generate(file.read())
+            plt.title("wordcloud_{}".format(i))
+            plt.imshow(wcloud)
+            plt.axis("off")
+            fig.tight_layout()
+            fig.savefig('select_six_wordplot_{}.png'.format(i))
+    
+    elif func == "special":
+        
+    
+    else:
+        print("No function was chosen for function algoTest().")
+        print("In the parameter of the function please specify \'func=(functionName)\' ")
     '''
     #write to file code
     c = g.community_infomap()
@@ -123,7 +218,7 @@ def algoTest(g):
     # print("Number of vertices in largest cluster:" + str(len(giant.vs)))
     # print("\t" + str(len(giant.vs)/len(g.vs)*100) + "% of all the verticies in the graph.")
     '''
-    #c = g.clusters()
+    # c = g.clusters()
     i = g.community_infomap()
     print(i)
     colors = ["#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00"]
@@ -153,27 +248,11 @@ def algoTest(g):
         # print(vertex)
     #plot(giant, vertex_color=["red" if type == 0 else "blue" for type in g.vs["type"]], vertex_label=[name for name in giant.vs["idx"]], edge_width = [weight for weight in giant.es["weight"]], edge_label = [weight for weight in giant.es["weight"]])
 
-    '''
-    #top 1% of degree code:
-    degrees_of_users = []
-    for i, vertex in enumerate(g.vs):
-        #print(i, vertex)
-        if g.vs[i]["type"] == 0:
-            degrees_of_users.append((vertex["idx"], g.degree(i)))
-            
-    # degrees_of_users.sort(key=lambda x: x[1]) ......sorting method below is faster?
-    degrees_of_users.sort(key=operator.itemgetter(1), reverse=True)
-    topPercentNum = int(len(degrees_of_users)/100)
+    # change to elif later!!
+
+def generateWordCloud():
+    print("placeholder")
     
-    print("Top 1% of users with highest degree")
-    for i in range(topPercentNum):
-        print(str(i) + ") " + str(degrees_of_users[i]))
-    
-    # double check result
-    # print("All users degree:")
-    # for element in degrees_of_users:
-        # print(element[1])
-    '''
 def buildCDF(list, filename):
     clusterSizes = list    
     for i, element in enumerate(clusterSizes):
@@ -269,14 +348,29 @@ def distributionFunc(g, func="CDF-Degree", filename="", filenameSeperator=",", s
         print("In the parameter of the function please specify either \'func=\"CDF-(type)\"\' or \'func=\"PDF-(type)\"\'")
         print("--- No output ---")
     
+def clean(postContent):
+    postContent = re.sub(r'(\\t)*(\\n)*','',postContent) #removes all \n and \t
+    soup = BeautifulSoup(postContent, "html.parser")
+    postContent = soup.get_text(separator=" ").replace("<br>", " ").lower() #lowercase it all to make searching easier
+#     print(postContent.encode("utf-8")) #for testing purposes
+    return postContent
+    
     
 if __name__ == "__main__":
     start_time = time.time()
-    g = buildGraph()
+    # g = buildGraph(save=True)
     # plot(g, vertex_color=[color for color in g.vs["color"]],  edge_width = [weight for weight in g.es["weight"]])
-    # algoTest(g)
+    # clusters = g.clusters()
+    # algoTest(clusters.giant(), func="fastGreedy")
+    # algoTest(g=None, func="getPostContentFromEdgeList")
+    # algoTest(g=None, func="wordcloud")
+    algoTest(g=None, func="special")
+    
     # distributionFunc(g, func="CDF-File", filename="fastGreedy_cluster_lenghts.txt", filenameSeperator='\n')
-    distributionFunc(g, func="CDF-Degree")
+    # distributionFunc(g, func="CDF-Degree")
+    
+    # g = pickle.load( open( "save.p", "rb" ) )
+    # summary(g)
 
 
     print("done: --- %s seconds ---" % (time.time() - start_time))
