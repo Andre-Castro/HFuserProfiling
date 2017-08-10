eef9e3c6730abb21a00f7198a100fb0c5966cb88
Andre-Castro <acast050@ucr.edu>
2017-08-10 14:29:03 -0700

    Updated code

--- test2.py
+++ test2.py
@@ -1,3 +1,4 @@
+#Andre's Messy Code....
 from igraph import *
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -16,6 +17,7 @@ from bs4 import BeautifulSoup
 
 # NOTE TO SELF:
 # FALSE == 0, TRUE != 0
+#USER = RED, THREAD = BLUE
 
 def buildGraph(sizeLimit=False, limitNum=500, save=False):
     usernameDict = {}
@@ -170,24 +172,157 @@ def algoTest(g, func=None):
             
             print("Done with {}".format(i))
     
-    elif func == "wordcloud":
-         for i in range(1,7):
-            file = open('select_six_post_content_{}.txt'.format(i), 'r', encoding="utf-8")
-            stopWordsFile = open('stopwords.txt', 'r')
+    # elif func == "wordcloud":
+         # for i in range(1,7):
+            # file = open('select_six_post_content_{}.txt'.format(i), 'r', encoding="utf-8")
+            # stopWordsFile = open('stopwords.txt', 'r')
             
-            fig = plt.figure(figsize=(20, 10))
-            ax  = fig.add_subplot('111')
+            # fig = plt.figure(figsize=(20, 10))
+            # ax  = fig.add_subplot('111')
             
-            wcloud = WordCloud(background_color="white", mode = "RGB", width=2000, height = 1000, stopwords = stopWordsFile.read().split('\n')).generate(file.read())
-            plt.title("wordcloud_{}".format(i))
-            plt.imshow(wcloud)
-            plt.axis("off")
-            fig.tight_layout()
-            fig.savefig('select_six_wordplot_{}.png'.format(i))
+            # wcloud = WordCloud(background_color="white", mode = "RGB", width=2000, height = 1000, stopwords = stopWordsFile.read().split('\n')).generate(file.read())
+            # plt.title("wordcloud_{}".format(i))
+            # plt.imshow(wcloud)
+            # plt.axis("off")
+            # fig.tight_layout()
+            # fig.savefig('select_six_wordplot_{}.png'.format(i))
     
-    elif func == "special":
+    # elif func == "wordcloud2":
+        # DBname = "MichalisPrj"
+        # pword = "1"
+        # tableName = "tbl_hackthissite_post"
+        # con = psycopg2.connect(database=DBname, user = "postgres", password=pword) #connect to postgres
+        # curs = con.cursor()
         
+        # for num in range(3,4):
+            # c1 = pickle.load(open("graph-and-edges\select_six_{}.pickle".format(num), "rb"))
+            # c1_degreeList = c1.degree()
+            # total = np.sum(c1_degreeList)
+            # degreeList = []
+            
+            # for i, degree in enumerate(c1_degreeList):
+                # percentage = (degree/total)*100
+                # if c1.vs[i]["type"] == 1:
+                    # degreeList.append((percentage, c1.vs[i]["idx"]))
+                    
+            # degreeList.sort(key=operator.itemgetter(0), reverse=True)
+            
+            # plotString = ""
+            
+            # 1 has 9 significant threads
+            # 2 has 16 significant threads
+            # 3 has 12 significant threads EXCLUDING the first one (outlier)
+            # 4 has 5 significant threads
+            # 5 has 10 significant threads
+            # 6 has 11 significant threads
+            
+            # fix this later to actually calculate the significant number of threads
+            # significant_threads = 12
+            
+            # for strng in degreeList[1:significant_threads]:
+                # tid = strng[1][4:]
+                # ExecuteStatement = "SELECT post_content FROM " + tableName + " WHERE tid = '{}'".format(tid)
+                # curs.execute(ExecuteStatement)
+                # postList = curs.fetchall()
+                # for post in postList:
+                    # cleanStr = clean(str(post))
+                    # cleanStr = cleanStr[3:-4]
+                    # plotString += cleanStr + "\n"
+                
+            
+            # stopWordsFile = open('stopwords.txt', 'r')
+            
+            # fig = plt.figure(figsize=(20, 10))
+            # ax  = fig.add_subplot('111')
+            
+            # wcloud = WordCloud(background_color="white", mode = "RGB", width=2000, height = 1000, stopwords = stopWordsFile.read().split('\n')).generate(plotString)
+            # plt.title("wordcloud_{}".format(num))
+            # plt.imshow(wcloud)
+            # plt.axis("off")
+            # fig.tight_layout()
+            # fig.savefig('significant_threads_wordplot_{}.png'.format(num))
     
+    elif func == "vertexDegreeDistribuion":
+        for i in range(1,7):
+            c1 = pickle.load(open("graph-and-edges\select_six_{}.pickle".format(i), "rb"))
+            filename = "cluster_{}_vertex_degree_distribution_tid.png".format(i)
+            summary(c1)
+            c1_degreeList = c1.degree()
+            total = np.sum(c1_degreeList)
+            # printList = []
+            x = []
+            y = []
+            x2 = []
+            y2 = []
+            
+            for i, degree in enumerate(sorted(c1_degreeList)):
+                percentage = (degree/total)*100
+                if c1.vs[i]["type"] == 0:
+                    x.append(i)
+                    y.append(percentage)
+                else:
+                    x2.append(i)
+                    y2.append(percentage)
+                # if percentage > 1:
+                    # printList.append(c1.vs[i]["idx"])
+                    # printList.append(str(percentage) + "%")
+            
+            fig = plt.figure(figsize=(8, 6))
+            ax = fig.add_subplot(111)
+            
+            ax.plot(x2, y2, 'bo')
+            ax.set_xlabel('num')
+            ax.set_ylabel('percentage')
+            
+            fig.tight_layout()
+            fig.savefig(filename)
+            
+    elif func == "printSigThreads":
+        DBname = "MichalisPrj"
+        pword = "1"
+        tableName = "tbl_hackthissite_post"
+        con = psycopg2.connect(database=DBname, user = "postgres", password=pword) #connect to postgres
+        curs = con.cursor()
+        
+        for num in range(1,7):
+            c1 = pickle.load(open("graph-and-edges\select_six_{}.pickle".format(num), "rb"))
+            c1_degreeList = c1.degree()
+            total = np.sum(c1_degreeList)
+            degreeList = []
+            
+            for i, degree in enumerate(c1_degreeList):
+                percentage = (degree/total)*100
+                if c1.vs[i]["type"] == 1:
+                    degreeList.append((percentage, c1.vs[i]["idx"]))
+                    
+            degreeList.sort(key=operator.itemgetter(0), reverse=True)
+            
+            plotString = ""
+            
+            #1 has 6 significant threads
+            #2 has 3 significant threads
+            #3 has 5 significant threads EXCLUDING the first one (outlier)
+            #4 has 6 significant threads
+            #5 has 7 significant threads
+            #6 has 6 significant threads
+            sig_num_threads_dict = {
+                1: [0,6],
+                2: [0,3],
+                3: [1,6], #1 for the outlier, six to push everything back by 1
+                4: [0,6],
+                5: [0,7],
+                6: [0,6],
+            }
+            #fix this later to actually calculate the significant number of threads
+            
+            print("Clutser {}:".format(num))
+            for strng in degreeList[sig_num_threads_dict[num][0]:sig_num_threads_dict[num][1]]:
+                tid = strng[1][4:]
+                print("\t" + tid)
+                
+    elif func == "special":
+        print("temporary")
+        
     else:
         print("No function was chosen for function algoTest().")
         print("In the parameter of the function please specify \'func=(functionName)\' ")
@@ -251,7 +386,84 @@ def algoTest(g, func=None):
     # change to elif later!!
 
 def generateWordCloud():
-    print("placeholder")
+    for i in range(1,7):
+        file = open('select_six_post_content_{}.txt'.format(i), 'r', encoding="utf-8")
+        stopWordsFile = open('stopwords.txt', 'r')
+        
+        fig = plt.figure(figsize=(20, 10))
+        ax  = fig.add_subplot('111')
+        
+        wcloud = WordCloud(background_color="white", mode = "RGB", width=2000, height = 1000, stopwords = stopWordsFile.read().split('\n')).generate(file.read())
+        plt.title("wordcloud_{}".format(i))
+        plt.imshow(wcloud)
+        plt.axis("off")
+        fig.tight_layout()
+        fig.savefig('select_six_wordplot_{}.png'.format(i))
+        
+def generateWordCloud2():
+    DBname = "MichalisPrj"
+    pword = "1"
+    tableName = "tbl_hackthissite_post"
+    con = psycopg2.connect(database=DBname, user = "postgres", password=pword) #connect to postgres
+    curs = con.cursor()
+    
+    for num in range(1,7):
+        c1 = pickle.load(open("graph-and-edges\select_six_{}.pickle".format(num), "rb"))
+        c1_degreeList = c1.degree()
+        total = np.sum(c1_degreeList)
+        degreeList = []
+        
+        for i, degree in enumerate(c1_degreeList):
+            percentage = (degree/total)*100
+            if c1.vs[i]["type"] == 1:
+                degreeList.append((percentage, c1.vs[i]["idx"]))
+                
+        degreeList.sort(key=operator.itemgetter(0), reverse=True)
+        
+        plotString = ""
+        
+        #1 has 6 significant threads
+        #2 has 3 significant threads
+        #3 has 5 significant threads EXCLUDING the first one (outlier)
+        #4 has 6 significant threads
+        #5 has 7 significant threads
+        #6 has 6 significant threads
+        sig_num_threads_dict = {
+            1: [0,6],
+            2: [0,3],
+            3: [1,6], #1 for the outlier, six to push everything back by 1
+            4: [0,6],
+            5: [0,7],
+            6: [0,6],
+        }
+        #fix this later to actually calculate the significant number of threads
+        
+        for strng in degreeList[sig_num_threads_dict[num][0]:sig_num_threads_dict[num][1]]:
+            tid = strng[1][4:]
+            ExecuteStatement = "SELECT post_content FROM " + tableName + " WHERE tid = '{}'".format(tid)
+            curs.execute(ExecuteStatement)
+            postList = curs.fetchall()
+            for post in postList:
+                cleanStr = clean(str(post))
+                cleanStr = cleanStr[3:-4]
+                plotString += cleanStr + "\n"
+            
+        
+        stopWordsFile = open('stopwords.txt', 'r')
+        
+        fig = plt.figure(figsize=(20, 10))
+        ax  = fig.add_subplot('111')
+        
+        wcloud = WordCloud(background_color="white", mode = "RGB", width=2000, height = 1000, stopwords = stopWordsFile.read().split('\n')).generate(plotString)
+        sorted_wcloudWords = sorted(wcloud.words_.items(), key=operator.itemgetter(1), reverse = True)
+        file = open('wordCloud_{}_Topics.txt'.format(num), 'w')
+        for word in sorted_wcloudWords:
+            file.write(word[0] + "\n")
+        plt.title("wordcloud_{}".format(num))
+        plt.imshow(wcloud)
+        plt.axis("off")
+        fig.tight_layout()
+        fig.savefig('significant_threads_wordplot_{}.png'.format(num))
     
 def buildCDF(list, filename):
     clusterSizes = list    
@@ -363,8 +575,9 @@ if __name__ == "__main__":
     # clusters = g.clusters()
     # algoTest(clusters.giant(), func="fastGreedy")
     # algoTest(g=None, func="getPostContentFromEdgeList")
-    # algoTest(g=None, func="wordcloud")
-    algoTest(g=None, func="special")
+    # algoTest(g=None, func="vertexDegreeDistribuion")
+    algoTest(g=None, func="printSigThreads")
+    # generateWordCloud2()
     
     # distributionFunc(g, func="CDF-File", filename="fastGreedy_cluster_lenghts.txt", filenameSeperator='\n')
     # distributionFunc(g, func="CDF-Degree")
